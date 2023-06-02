90e777007b1f3f52a37e124bfbdd9715ae39c5ca
Bretton Wade <bretton.wade@irdev.us>
2023-06-02 09:28:23 -0400

    development - export updates for those places that need to access them

--- Java/applications/bedrock-site/src/main/java/module-info.java
+++ Java/applications/bedrock-site/src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module us.irdev.bedrock.site {
-    requires us.irdev.bedrock.bag;
-    requires us.irdev.bedrock.service;
-    requires us.irdev.bedrock.secret;
     requires jakarta.servlet;
+
+    requires us.irdev.bedrock.bag;
     requires us.irdev.bedrock.logger;
+    requires us.irdev.bedrock.secret;
+    requires us.irdev.bedrock.service;
+
     opens us.irdev.bedrock.site to us.irdev.bedrock.service;
 }
--- Java/libraries/bag/src/main/java/module-info.java
+++ Java/libraries/bag/src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module us.irdev.bedrock.bag {
     requires jdk.unsupported;
+
     requires us.irdev.bedrock;
     requires us.irdev.bedrock.logger;
+
     exports us.irdev.bedrock.bag;
+    exports us.irdev.bedrock.bag.entry;
     exports us.irdev.bedrock.bag.formats;
 }
